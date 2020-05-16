5ae54d877cff0f3be28211bb7a9147ee2722a7d1
Devika <devikapatadia9@gmail.com>
2020-05-16 23:45:55 +0530

    Enter

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -10,133 +10,153 @@ import 'package:flare_flutter/flare_actor.dart';
 
 class HomeScreen extends StatelessWidget {
   static GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-  final appBar = AppBar(
-    centerTitle: true,
-    elevation: 0,
-    backgroundColor: Colors.white,
-    leading: IconButton(
-      onPressed: () {
-        _scaffoldKey.currentState.openDrawer();
-      },
-      icon: Icon(
-        Icons.dashboard,
-        color: Colors.black,
-        size: 40,
-      ),
-    ),
-    title: Image(
-      height: 75,
-      image: AssetImage('images/logo.png'),
-    ),
-  );
+
   @override
   Widget build(BuildContext context) {
-    double padd = (MediaQuery.of(context).size.width * 0.01);
+    // double padd = (MediaQuery.of(context).size.width * 0.01);
     return Scaffold(
-        key: _scaffoldKey,
-        appBar: appBar,
-        drawer: Drawer(
-          child: Container(
-            color: Color(0xFF666666),
-            child: DrawerWidget(),
+      key: _scaffoldKey,
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            _scaffoldKey.currentState.openDrawer();
+          },
+          icon: Icon(
+            Icons.dashboard,
+            color: Colors.black,
+            size: 40,
           ),
         ),
-        backgroundColor: Colors.white,
-        body: Container(
-          height: (MediaQuery.of(context).size.height -
-              appBar.preferredSize.height -
-              MediaQuery.of(context).padding.top),
-          child: ListView(children: [
-            SizedBox(
-              height: (MediaQuery.of(context).size.height -
-                      appBar.preferredSize.height -
-                      MediaQuery.of(context).padding.top) *
-                  0.01,
-            ),
-            Container(
-              height: (MediaQuery.of(context).size.height -
-                      appBar.preferredSize.height -
-                      MediaQuery.of(context).padding.top) *
-                  0.3,
-              child: Cards(
-                colour: Color(0xFFffbed1),
-                childCard: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    SizedBox(
-                      width: (MediaQuery.of(context).size.width * 0.01),
+        title: Image(
+          height: 75,
+          image: AssetImage('images/logo.png'),
+        ),
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Color(0xFF666666),
+          child: DrawerWidget(),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 200,
+            width: double.infinity,
+            child: Cards(
+              colour: Color(0xFFffbed1),
+              childCard: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: TyperAnimatedTextKit(
+                      text: ['VIRTUAL\nLABS'],
+                      textStyle: kvirtual_lab,
+                      speed: Duration(milliseconds: 200),
+                      isRepeatingAnimation: false,
                     ),
-                    Container(
-                      width: (MediaQuery.of(context).size.width * 0.49),
-                      child: TyperAnimatedTextKit(
-                        text: ['VIRTUAL\nLABS'],
-                        textStyle: kvirtual_lab,
-                        speed: Duration(milliseconds: 200),
-                        isRepeatingAnimation: false,
-                      ),
-                    ),
-                    Container(
-                      width: (MediaQuery.of(context).size.width * 0.39),
-                      child: BounceInRight(
-                        child: FlareActor(
-                          'images/Lab Final.flr',
-                          animation: 'lab',
-                          fit: BoxFit.fitWidth,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            //end of first
-
-            Container(
-              height: (MediaQuery.of(context).size.height -
-                  appBar.preferredSize.height -
-                  MediaQuery.of(context).padding.top * 0.1),
-              child: Padding(
-                padding: EdgeInsets.only(left: padd),
-                child: Text(
-                  'Standard VII',
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'DustismoRomanBold'),
-                ),
-              ),
-            ),
-            Container(
-               height: (MediaQuery.of(context).size.height -
-                      appBar.preferredSize.height -
-                      MediaQuery.of(context).padding.top) *
-                  0.25,
-              child: GestureDetector(
-                child: Cards(
-                  colour: Color(0xFFbdd4ff),
-                  childCard: CardChild(
-                    text: '1. Growing \n plants from \n seeds',
-                    wid: BounceInRight(
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: BounceInRight(
                       child: FlareActor(
-                        'images/Plant new 3.flr',
-
-                        //fit: BoxFit.contain,
-
-                        animation: "plant",
+                        'images/Lab Final.flr',
+                        animation: 'lab',
+                        fit: BoxFit.fitWidth,
                       ),
                     ),
                   ),
-                ),
-                onTap: () {
-                  Navigator.pushNamed(context, '/growingplants');
-                },
+                ],
               ),
             ),
-          ]),
-        ));
+          ),
+          //end of first
+        ],
+      ),
+    );
   }
 }
+
+class Entry {
+  Entry(this.title, [this.children = const <Entry>[]]);
+
+  final String title;
+  final List<Entry> children;
+}
+
+final List<Entry> data = <Entry>[
+  Entry(
+    'Standard VII',
+    <Entry>[
+      Entry(
+        'Section A0',
+        <Entry>[
+          Entry('Item A0.1'),
+          Entry('Item A0.2'),
+          Entry('Item A0.3'),
+        ],
+      ),
+      Entry('Section A1'),
+      Entry('Section A2'),
+    ],
+  ),
+];
+
+class EntryItem extends StatelessWidget {
+  final Entry entry;
+
+  EntryItem(this.entry);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+// Container(
+//   height:,
+//   child: Padding(
+//     padding: EdgeInsets.only(left: 10),
+//     child: Text(
+//
+//       textAlign: TextAlign.start,
+//       style: TextStyle(
+//           fontSize: 28,
+//           fontWeight: FontWeight.bold,
+//           fontFamily: 'DustismoRomanBold'),
+//     ),
+//   ),
+// ),
+// Container(
+
+//   child: GestureDetector(
+//     child: Cards(
+//       colour: Color(0xFFbdd4ff),
+//       childCard: CardChild(
+//         text: '1. Growing \n plants from \n seeds',
+//         wid: BounceInRight(
+//           child: FlareActor(
+//             'images/Plant new 3.flr',
+//             animation: "plant",
+//           ),
+//         ),
+//       ),
+//     ),
+//     onTap: () {
+//       Navigator.pushNamed(context, '/growingplants');
+//     },
+//   ),
+// ),
 
 //////
 
