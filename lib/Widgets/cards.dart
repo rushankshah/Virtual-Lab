3c153027f79423296816d7b15fa88613a49ca9c2
Rushank Shah <rushankshah65@gmail.com>
2020-05-17 22:46:35 +0530

    Some minor changes added

--- lib/Widgets/cards.dart
+++ lib/Widgets/cards.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
 
 class Cards extends StatelessWidget {
-  final int height;
-  final int width;
   final Color colour;
   final Widget childCard;
   final double radius;
   final double l, t, r, b, height;
 
-
-  Cards(
-      {this.colour,
-      this.height=160,
-      this.width,
-      this.childCard,
-      this.radius = 37,
-      this.t = 10,
-      this.r = 10,
-      this.l = 10,
-      this.b = 10,
-      this.height = 160});
+  Cards({
+    this.colour,
+    this.height = 160,
+    this.childCard,
+    this.radius = 37,
+    this.t = 10,
+    this.r = 10,
+    this.l = 10,
+    this.b = 10,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +24,6 @@ class Cards extends StatelessWidget {
       height: height,
       child: childCard,
       margin: EdgeInsets.fromLTRB(l, t, r, b),
-      width: double.infinity,
       decoration: BoxDecoration(
           color: colour, borderRadius: BorderRadius.circular(radius)),
     );
