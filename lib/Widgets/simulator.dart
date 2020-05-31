44d8de30fa54d745319813c79c94237eb366680d
Rushank Shah <rushankshah65@gmail.com>
2020-05-31 23:21:50 +0530

    Done a few things on the simulator

--- lib/Widgets/simulator.dart
+++ lib/Widgets/simulator.dart
@@ -1,58 +1,116 @@
+import 'package:flare_dart/math/mat2d.dart';
+import 'package:flare_flutter/flare.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flare_flutter/flare_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:turing/Widgets/animation_box.dart';
 
 import 'cards.dart';
 
+class SimulatorController extends FlareController {
+  @override
+  bool advance(FlutterActorArtboard artboard, double elapsed) {
+    // TODO: implement advance
+    throw UnimplementedError();
+  }
+
+  @override
+  void initialize(FlutterActorArtboard artboard) {
+    // TODO: implement initialize
+  }
+
+  @override
+  void setViewTransform(Mat2D viewTransform) {
+    // TODO: implement setViewTransform
+  }
+}
+
 class Simulator extends StatefulWidget {
   final int std, theme;
   Simulator({this.std, this.theme});
   @override
-  _SimulatorState createState() => _SimulatorState(std, theme);
+  _SimulatorState createState() => _SimulatorState();
 }
 
 class _SimulatorState extends State<Simulator>
     with AutomaticKeepAliveClientMixin<Simulator> {
-  final int std, theme;
-  int test = 1;
-  _SimulatorState(this.std, this.theme);
-  bool isEnabled() {
-    if (1 == test)
-      return true;
-    else
-      return false;
-  }
-
+  SimulatorController _simulatorController;
+  double borderRadius = 22, height = 50, radius = 20;
+  String flareFileDirectory, animationName;
+  String selectedSeed = 'Grams';
   @override
+  // ignore: must_call_super
   Widget build(BuildContext context) {
-    if (std == 7) {
-      if (theme == 1) {
-        print('Standard 7 Theme 1');
+    if (widget.std == 7) {
+      if (widget.theme == 1) {
+        flareFileDirectory = 'images/Theme1Activity1Step1.flr';
+        animationName = 'bowl';
       }
     }
     return ListView(
       children: <Widget>[
         AnimationBox(
           animation: Center(
-            child: Text('Animations will appear over here'),
+            child: FlareActor(
+              flareFileDirectory,
+              animation: animationName,
+              controller: _simulatorController,
+            ),
           ),
         ),
-        MaterialButton(
+        Container(
+          child: Center(
+            child: DropdownButton(
+              value: selectedSeed,
+              items: [
+                DropdownMenuItem(
+                  child: Text('Grams'),
+                  value: 'Grams',
+                ),
+                DropdownMenuItem(
+                  child: Text('Moong'),
+                  value: 'Moong',
+                )
+              ],
+              onChanged: (value) {
+                setState(() {
+                  selectedSeed = value;
+                });
+              },
+            ),
+          ),
+        ),
+        FlatButton(
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(22.0),
+            borderRadius: BorderRadius.circular(borderRadius),
           ),
           child: Cards(
-            height: 50,
-            radius: 30,
-            colour: isEnabled() ? Colors.red[300] : Colors.red[100],
-            childCard: Center(child: Text('Button Example')),
+            height: height,
+            radius: radius,
+            colour: Color(0xFFbdd4ff),
+            childCard: Center(child: Text('Add Seeds')),
           ),
-          onPressed: isEnabled()
-              ? () {
-                  setState(() {
-                    test = test == 0 ? 1 : 0;
-                  });
-                }
-              : null,
+          onPressed: () {
+            setState(() {
+              animationName = 'Seeds';
+            });
+          },
+        ),
+        FlatButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+          child: Cards(
+            height: height,
+            radius: radius,
+            colour: Color(0xFFffff99),
+            childCard: Center(child: Text('Add Water')),
+          ),
+          onPressed: () {
+            setState(() {
+              animationName = 'water1';
+            });
+          },
         )
       ],
     );
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+
 const String dustismo = 'DustismoBold';
+const String dustismoRomanBold = 'DustismoRomanBold';
 const String frostBiteNarrow = 'FrostBiteNarrow';
 
 const kVirtualLabText = TextStyle(
     fontWeight: FontWeight.w500, fontSize: 35, fontFamily: frostBiteNarrow);
 
-const kCardText = TextStyle(
-    fontWeight: FontWeight.w500, fontSize: 28, fontFamily: dustismo);
+const kCardText =
+    TextStyle(fontWeight: FontWeight.w500, fontSize: 28, fontFamily: dustismo);
 
 const kDrawerWidgetText = TextStyle(
     fontWeight: FontWeight.w500,
@@ -14,17 +16,17 @@ const kDrawerWidgetText = TextStyle(
     fontFamily: frostBiteNarrow,
     color: Colors.white);
 
-const kStandardDetailText = TextStyle(
-    fontSize: 28, fontWeight: FontWeight.bold, fontFamily: dustismo);
+const kStandardDetailText =
+    TextStyle(fontSize: 28, fontWeight: FontWeight.bold, fontFamily: dustismo);
 
 const kOnScreenStandardDetailText = TextStyle(
-    fontSize: 23, fontWeight: FontWeight.bold, fontFamily: dustismo);
+    fontSize: 23, fontWeight: FontWeight.bold, fontFamily: dustismoRomanBold);
 
 const kActivityText = TextStyle(fontSize: 25, fontFamily: dustismo);
 const kTabBarText = TextStyle(color: Colors.black, fontSize: 15);
 const loadingScreenText = TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.w100,
-                fontFamily: 'DustismoBold',
-              );
+  color: Colors.black,
+  fontSize: 20,
+  fontWeight: FontWeight.w100,
+  fontFamily: 'DustismoBold',
+);
